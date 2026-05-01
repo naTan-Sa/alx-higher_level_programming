77ab05947714b6b4f76c78fe73222339541e90fd
naTan-Sa <natansahlu15@gmail.com>
2026-05-01 08:54:33 +0000

    Task 0: Import add function and print result using variables and string formatting

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Import add function and print result."""
-
-
-from add_0 import add
-
 
 if __name__ == "__main__":
+    """Import add function and print result."""
+
+
+    from add_0 import add
+
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
