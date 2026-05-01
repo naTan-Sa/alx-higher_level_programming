c11315c489f0c1449f60d34c16184e2b740b5792
naTan-Sa <natansahlu15@gmail.com>
2026-05-01 18:07:30 +0000

    Task 3: Add all command line arguments and print result

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Add all command line arguments."""
+
+import sys
+
+if __name__ == "__main__":
+    total = 0
+
+    for i in range(1, len(sys.argv)):
+        total += int(sys.argv[i])
+
+    print(total)
