a920212c523c42242a7cf0f821f46512f54338e5
naTan-Sa <natansahlu15@gmail.com>
2026-05-01 14:27:11 +0000

    Task 2: Print number and list of command line arguments dynamically

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
 
-
 if __name__ == "__main__":
-    """Print the number and list of arguments."""
+    """Print the number of and list of arguments."""
     import sys
 
-
-    args = sys.argv
-    count = len(args) - 1
-
+    count = len(sys.argv) - 1
     if count == 0:
-        print("0 arguments: ")
+        print("0 arguments.")
     elif count == 1:
-        print("1 arguments: ")
+        print("1 argument:")
     else:
-        print("{} arguments: ".format(count))
-
-    for i in range(1, len(args)):
-        print("{} : {}".format(i, args[i]))
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
